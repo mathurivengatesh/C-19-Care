47ab736b24dca6d293355c99d24d7b8d39e335ca
Mathuri V <math3025@global.chainsys.com>
2022-06-08 15:32:41 +0530

    included all files

--- Mini-project/server/data.js
+++ Mini-project/server/data.js
@@ -7,7 +7,7 @@ const password = "58de0ca6ebd4250a97d0a7d300191f68";
 const _tmp = "name";
 const cloudant = Cloudant({ url: url, username: username, password: password });
 
-insert = function (paramsvalue) {
+const insert = function (paramsvalue) {
   console.log(paramsvalue);
   cloudant
     .use("c_19_care")
@@ -19,10 +19,10 @@ insert = function (paramsvalue) {
       console.log(err);
     });
 };
-get=function(dbname){
+const get=function(dbname){
   return cloudant.use(dbname).list();
 }
-getId= function(value,db){
+const getId= function(value,db){
   return cloudant.use(db).get(value)
 }
 module.exports={insert,get,getId};
