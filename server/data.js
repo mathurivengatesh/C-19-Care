73698d826c3350d86f7fe88326a2fcfb4e1ca6e4
Mathuri V <math3025@global.chainsys.com>
2022-06-16 10:00:42 +0530

    include

--- server/data.js
+++ server/data.js
@@ -2,10 +2,12 @@ let Cloudant = require("@cloudant/cloudant");
 let { type } = require("express/lib/response");
 let url =
  "https://75c481c7-3349-4ad5-86c0-311dd22187eb-bluemix.cloudantnosqldb.appdomain.cloud/";
-let username = "apikey-v2-2mxwaz89u58vkezj2e5jfc41xn3komuaq1j49fhhmu8p";
-let password = "58de0ca6ebd4250a97d0a7d300191f68";
+ const loginCred ={
+username : "apikey-v2-2mxwaz89u58vkezj2e5jfc41xn3komuaq1j49fhhmu8p",
+password : "58de0ca6ebd4250a97d0a7d300191f68"
+ };
 let _tmp = "name";
-let cloudant = Cloudant({ url: url, username: username, password: password });
+let cloudant = Cloudant({ url: url, username: loginCred.username, password: loginCred.password });
 
 let insert = function (paramsvalue) {
   return cloudant
